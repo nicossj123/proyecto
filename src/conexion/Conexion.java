dfaa6da3ca30bb7b8430d12c32e4f80168a3ae9d
TonyMontanaPC <TonyMontanaPC@DESKTOP-ELIIVKN>
2021-11-24 13:07:41 -0300

    Primera subida

---
+++ src/conexion/Conexion.java
@@ -0,0 +1,28 @@
+
+package conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection obtenerConexion(){
+        Connection conec = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conec = DriverManager.getConnection("jdbc:mysql://localhost:3306/libreria","root","");
+            System.out.println("Conexion exitosa!");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Error en conexion "+ e.getMessage());
+        }
+        return conec;
+    }
+    public static void main(String[] args) {
+        Conexion cnx = new Conexion();
+        cnx.obtenerConexion();
+    }
+
+}
+
+
