be4c1fbaf4466374d8849dcaf487710552acbd40
tomaz <tomaz@DESKTOP-H25IAHN>
2021-11-24 13:45:51 -0300

    creación clases equipopartido y jugadorpartido

---
+++ src/conexion/EquipoPartido.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+/**
+ *
+ * @author tomaz
+ */
+public class EquipoPartido {
+    private int equipo_id_equipo,partido_id_partido;
+
+    public EquipoPartido() {
+    }
+
+    public EquipoPartido(int equipo_id_equipo, int partido_id_partido) {
+        this.equipo_id_equipo = equipo_id_equipo;
+        this.partido_id_partido = partido_id_partido;
+    }
+
+    public int getEquipo_id_equipo() {
+        return equipo_id_equipo;
+    }
+
+    public void setEquipo_id_equipo(int equipo_id_equipo) {
+        this.equipo_id_equipo = equipo_id_equipo;
+    }
+
+    public int getPartido_id_partido() {
+        return partido_id_partido;
+    }
+
+    public void setPartido_id_partido(int partido_id_partido) {
+        this.partido_id_partido = partido_id_partido;
+    }
+
+    @Override
+    public String toString() {
+        return "EquipoPartido{" + "equipo_id_equipo=" + equipo_id_equipo + ", partido_id_partido=" + partido_id_partido + '}';
+    }
+
+
+}
---
+++ src/conexion/JugadorPartido.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexion;
+
+/**
+ *
+ * @author tomaz
+ */
+public class JugadorPartido {
+    private int jugador_id_jugador,partido_id_partido;
+
+    public JugadorPartido() {
+    }
+
+    public JugadorPartido(int jugador_id_jugador, int partido_id_partido) {
+        this.jugador_id_jugador = jugador_id_jugador;
+        this.partido_id_partido = partido_id_partido;
+    }
+
+    public int getJugador_id_jugador() {
+        return jugador_id_jugador;
+    }
+
+    public void setJugador_id_jugador(int jugador_id_jugador) {
+        this.jugador_id_jugador = jugador_id_jugador;
+    }
+
+    public int getPartido_id_partido() {
+        return partido_id_partido;
+    }
+
+    public void setPartido_id_partido(int partido_id_partido) {
+        this.partido_id_partido = partido_id_partido;
+    }
+
+    @Override
+    public String toString() {
+        return "JugadorPartido{" + "jugador_id_jugador=" + jugador_id_jugador + ", partido_id_partido=" + partido_id_partido + '}';
+    }
+}
