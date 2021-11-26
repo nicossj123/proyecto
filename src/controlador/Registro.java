f52d2d9f05beb95763a7ce6e8e35a00072923707
TonyMontanaPC <TonyMontanaPC@DESKTOP-ELIIVKN>
2021-11-26 17:25:19 -0300

    Quinta subida

--- src/controlador/Registro.java
+++ src/controlador/Registro.java
@@ -10,8 +10,10 @@ import java.sql.Connection;
 import conexion.Conexion;
 import java.sql.SQLException;
 import modelo.Arbitro;
+import modelo.Campeonato;
 import modelo.Equipo;
 import modelo.Jugador;
+import modelo.Partido;
 
 /**
  *
@@ -19,12 +21,12 @@ import modelo.Jugador;
  */
 public class Registro {
 
-    public boolean agregarJugador(Jugador jugador){
+    public boolean agregarJugador(Jugador jugador) {
 
         try {
             Conexion con = new Conexion();
             Connection cnx = con.obtenerConexion();
-            
+
             String query = "INSERT INTO jugador(id_jugador,pnombre,apaterno,posicion,equipo_id_equipo)VALUES (?,?,?,?,?)";
             PreparedStatement stmt = cnx.prepareStatement(query);
             stmt.setInt(1, jugador.getId_equipo());
@@ -41,20 +43,20 @@ public class Registro {
         } catch (SQLException e) {
             System.out.println("No se pudo insertar jugador " + e.getMessage());
             return false;
-        }            
+        }
     }
-    
-    public boolean agregarEquipo(Equipo equipo){
+
+    public boolean agregarEquipo(Equipo equipo) {
 
         try {
             Conexion con = new Conexion();
             Connection cnx = con.obtenerConexion();
 
-            String query = "INSERT INTO jugador(id_equipo,nombre,cancha_local)VALUES (?,?,?)";
+            String query = "INSERT INTO equipo(id_equipo,nombre,cancha_local)VALUES (?,?,?)";
             PreparedStatement stmt = cnx.prepareStatement(query);
             stmt.setInt(1, equipo.getId_equipo());
             stmt.setString(2, equipo.getNombre());
-            stmt.setString(3, equipo.getCancha_local());           
+            stmt.setString(3, equipo.getCancha_local());
 
             stmt.executeUpdate();
             cnx.close();
@@ -66,20 +68,19 @@ public class Registro {
             return false;
         }
 
-
     }
 
-        public boolean agregarArbitro(Arbitro arbitro){
+    public boolean agregarArbitro(Arbitro arbitro) {
 
         try {
             Conexion con = new Conexion();
             Connection cnx = con.obtenerConexion();
 
-            String query = "INSERT INTO jugador(id_arbitro,asignado,nombre)VALUES (?,?,?)";
+            String query = "INSERT INTO arbitro(id_arbitro,asignado,nombre)VALUES (?,?,?)";
             PreparedStatement stmt = cnx.prepareStatement(query);
             stmt.setInt(1, arbitro.getId_arbitro());
             stmt.setBoolean(2, arbitro.isAsignado());
-            stmt.setString(3, arbitro.getNombre());            
+            stmt.setString(3, arbitro.getNombre());
 
             stmt.executeUpdate();
             cnx.close();
@@ -89,9 +90,104 @@ public class Registro {
         } catch (SQLException e) {
             System.out.println("No se pudo insertar arbitro " + e.getMessage());
             return false;
-        }            
+        }
     }
 
-}
+    public boolean actualizarJugador(Jugador jugador) {
 
+        try {
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            String query = "UPDATE jugador set id_jugador=?,pnombre=?,apaterno=?,posicion=?,equipo_id_equipo=? WHERE id_jugador=?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, jugador.getId_jugador());
+            stmt.setString(2, jugador.getPnombre());
+            stmt.setString(3, jugador.getApaterno());
+            stmt.setString(4, jugador.getPosicion());
+            stmt.setInt(5, jugador.getId_equipo());
+            
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            return true;
+        } catch (SQLException e) {
+            System.out.println("No se pudo actualizar jugador " + e.getMessage());
+            return false;
+        }        
+   }
+    
+    public boolean actualizarEquipo(Equipo equipo) {
+
+        try {
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            String query = "UPDATE equipo set id_equipo=?,nombre=?,cancha_local=? WHERE id_equipo=?";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, equipo.getId_equipo());
+            stmt.setString(2, equipo.getNombre());
+            stmt.setString(3, equipo.getCancha_local());
+           
+            
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            
+            return true;
+        } catch (SQLException e) {
+            System.out.println("No se pudo actualizar equipo " + e.getMessage());
+            return false;
+        }
+
+    }
+    
+    public boolean agregarEquipoCampeonato(Equipo equipo, Campeonato campeonato) {
+
+        try {
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+
+            String query = "INSERT INTO EquipoCampeonato(equipo_id_equipo,campeonato_id_campeonato)VALUES (?,?)";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, equipo.getId_equipo());
+            stmt.setInt(2, campeonato.getId_campeonato());
+            
+
+            stmt.executeUpdate();
+            cnx.close();
+            stmt.close();
+            System.out.println("Equipo insertado correctamente");
+            return true;
+        } catch (SQLException e) {
+            System.out.println("No se pudo insertar equipo al campeonato " + e.getMessage());
+            return false;
+        }
+    }
+     
+    public boolean agregarPartido(Partido partido){
+        try {
+            Conexion con = new Conexion();
+            Connection cnx = con.obtenerConexion();
+            
+            String query = "INSERT INTO partido(id_partido,duracion_min,id_campeonato,id_arbitro) VALUES (?,?,?)";
+            PreparedStatement stmt = cnx.prepareStatement(query);
+            stmt.setInt(1, partido.getId_partido());
+            stmt.setInt(2, partido.getDuracion_min());
+            stmt.setInt(3, partido.getDuracion_min());
+            stmt.setInt(4, partido.getId_arbitro());
+            
+            stmt.executeUpdate();
+            stmt.close();
+            cnx.close();
+            return true;
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+    
+    
+
+
+}
 
--- src/modelo/Campeonato.java
+++ src/modelo/Campeonato.java
@@ -4,15 +4,13 @@ package modelo;
 
 public class Campeonato {
     private int id_campeonato,cant_equipos;
-    private String equipo_campeonato;
 
     public Campeonato() {
     }
 
-    public Campeonato(int id_campeonato, int cant_equipos, String equipo_campeonato) {
+    public Campeonato(int id_campeonato, int cant_equipos) {
         this.id_campeonato = id_campeonato;
         this.cant_equipos = cant_equipos;
-        this.equipo_campeonato = equipo_campeonato;
     }
 
     public int getId_campeonato() {
@@ -30,14 +28,5 @@ public class Campeonato {
     public void setCant_equipos(int cant_equipos) {
         this.cant_equipos = cant_equipos;
     }
-
-    public String getEquipo_campeonato() {
-        return equipo_campeonato;
-    }
-
-    public void setEquipo_campeonato(String equipo_campeonato) {
-        this.equipo_campeonato = equipo_campeonato;
-    }
-    
-    
+              
 }
